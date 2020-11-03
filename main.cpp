11e8b915b5b8fb1fdf17e531e0440a8cc442247a
misimisi171718 <sumegimisi02@gmail.com>
2020-11-03 08:36:29 +0100

    bug fix but stil dosent work

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
       for (int z = distance * -1; z < distance; z++)
       {
          Pos temp = getStructurePos(SWAMP_HUT_CONFIG, seed, 3, 0, NULL);
-         if(isViableStructurePos(Swamp_Hut, MC_1_16, &g, seed, x, z))
+         if(isViableStructurePos(Swamp_Hut, MC_1_16, &g, seed, temp.x, temp.z))
             huts.push_back({x,z});
       }
    for (auto &&i : huts)
